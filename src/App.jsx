3fac938dfe1bba5773f83cb9cccc83cd70383c6e
Tanuj <khiladitanuj@gmail.com>
2024-12-18 20:09:30 +0530

    Resume page

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ResumePage from "./Pages/ResumePage.jsx";
+
+function App() {
+    return <>
+        <div className="">
+            <ResumePage/>
+        </div>
+    </>
+}
+
+export default App;
